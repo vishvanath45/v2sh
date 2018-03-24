ac57960befd1eca1fcc03ca09da7e9a3923d9cb3
hasan356 <hasan.150295@gmail.com>
2018-03-24 10:15:58 +0530

    Django init

---
+++ v2sh/alumini/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request , 'home.html')
