5136fb2eb8908d09128947b95524911a7393769c
hasan356 <hasan.150295@gmail.com>
2018-03-24 16:00:07 +0530

    Added models

--- v2sh/alumini/models.py
+++ v2sh/alumini/models.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique = True)
+    password = models.Field()
+
+
+class SuperUser(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    ph_no = models.TextField()
+    internship = models.TextField()
 # Create your models here.
