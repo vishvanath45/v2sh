3f528d636cf537d74b3233b282783f770041ec94
vishvanath45 <visitdutt@gmail.com>
2018-03-24 12:31:10 +0530

    Issue #3 fixed

--- v2sh/alumini/urls.py
+++ v2sh/alumini/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url,include
 from . import views
 
-urlpatterns = [
-    url(r'', views.home , name= 'home'),
+urlpatterns = [    
+    url(r'^$', views.home , name= 'home'),
+    url(r'^reportissue/$', views.reportissue, name = 'reportissue'),
 ]
