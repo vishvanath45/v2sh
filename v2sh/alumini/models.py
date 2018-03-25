039fb7b5e36f94d3e1c389210800671a4496e97d
vishvanath45 <visitdutt@gmail.com>
2018-03-26 02:40:03 +0530

    db designed

--- v2sh/alumini/models.py
+++ v2sh/alumini/models.py
@@ -9,11 +9,19 @@ class User(models.Model):
     email = models.EmailField(unique = True)
     password = models.Field()
 
-
 class SuperUser(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(primary_key = True)
     ph_no = models.TextField()
-    internship = models.TextField()
+    # internship = models.TextField()
+
+class Experience(models.Model):
+	company_name = models.CharField(max_length=100)
+	joining_date = models.DateField()
+	# take data in YYYY-MM-DD
+	ending_date = models.DateField()
+	role = models.CharField(max_length=100)
+	internship_or_job = models.BooleanField(default = True)
+	object_name = models.ForeignKey(SuperUser)
 
 # Create your models here.
