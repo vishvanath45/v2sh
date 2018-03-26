3c37af80fda5837425f615370d6366d7d2c39996
hasan356 <hasan.150295@gmail.com>
2018-03-26 14:42:55 +0530

    Adding database to admin section

--- v2sh/alumini/admin.py
+++ v2sh/alumini/admin.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+
+from .models import SuperUser,Experience
 # Register your models here.
+
+admin.site.register(SuperUser)
+
+admin.site.register(Experience)
--- v2sh/alumini/models.py
+++ v2sh/alumini/models.py
@@ -16,12 +16,12 @@ class SuperUser(models.Model):
     # internship = models.TextField()
 
 class Experience(models.Model):
-	company_name = models.CharField(max_length=100)
-	joining_date = models.DateField()
-	# take data in YYYY-MM-DD
-	ending_date = models.DateField()
-	role = models.CharField(max_length=100)
-	internship_or_job = models.BooleanField(default = True)
-	object_name = models.ForeignKey(SuperUser)
+    company_name = models.CharField(max_length=100)
+    joining_date = models.DateField()
+    # take data in YYYY-MM-DD
+    ending_date = models.DateField()
+    role = models.CharField(max_length=100)
+    internship_or_job = models.BooleanField(default = True)
+    object_name = models.ForeignKey(SuperUser)
 
 # Create your models here.
