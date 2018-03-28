5d125dedf1ee04d12e1f693920667c8e013fb39c
hasan356 <hasan.150295@gmail.com>
2018-03-28 16:18:27 +0530

    Added user model to admin section

--- v2sh/alumini/admin.py
+++ v2sh/alumini/admin.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 
-from .models import SuperUser,Experience
+from .models import SuperUser,Experience,User
 # Register your models here.
+
+admin.site.register(User)
 
 admin.site.register(SuperUser)
 
