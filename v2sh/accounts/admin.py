1cc6843ffe80f5e27fd9399e88f49452734ef053
hasan356 <hasan.150295@gmail.com>
2018-03-30 21:47:39 +0530

    Adding user to the database and added signup page

--- v2sh/accounts/admin.py
+++ v2sh/accounts/admin.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import User
+from .models import user
 # Register your models here.
-admin.site.register(User)
+admin.site.register(user)
---
+++ v2sh/accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import user
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    class Meta:
+        model = user
+        fields = ('name','email','password','confirm_password')
+
+    # Name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    # Email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    # Password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    # Confirm_Password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    #
