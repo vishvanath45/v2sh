9a23b1bd5da29414c98866f36b0361ea5f4249ad
hasan356 <hasan.150295@gmail.com>
2018-03-30 21:47:39 +0530

    Adding user to the database and added signup page

--- v2sh/v2sh/settings.py
+++ v2sh/v2sh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'superuser',
     'accounts',
     'contents',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGOUT_REDIRECT_URL = 'home'
