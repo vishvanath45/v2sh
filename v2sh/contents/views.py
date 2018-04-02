2bfa5d8036c56cfbefe63180b947a25e7a3a006c
rishi07 <07saptarshidey@gmail.com>
2018-04-02 17:14:56 +0530

    "search results with indexing"

--- v2sh/contents/views.py
+++ v2sh/contents/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from superuser.models import SuperUser, Experience
 # Create your views here.
 
 def home(request):
@@ -19,3 +20,22 @@ def feedbackform(request):
 
 def ourmission(request):
 	return render(request, 'contents/ourmission.html')
+
+def bycompany(request,alpha):
+
+	names=[]
+	number_of_people=[]
+	job_count_pairs = []
+	for i in range(Experience.objects.count()):
+		if(Experience.objects.all()[i].company_name[0]==alpha):
+			names.append(Experience.objects.all()[i].company_name)
+	for i in names:
+		cnt=0
+		for j in range(Experience.objects.count()):
+			if(str(i)== str(Experience.objects.all()[j].company_name)):
+				cnt+=1
+		number_of_people.append(cnt)
+	for i in range(len(names)):
+		job_count_pairs.append((names[i], number_of_people[i]))
+
+	return render(request,'contents/bycompany.html',{'names':job_count_pairs,'alpha':alpha})
