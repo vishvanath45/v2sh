fc97e9805c13986ff33637bff74b31f6f24e683f
hasan356 <hasan.150295@gmail.com>
2018-04-07 00:37:44 +0530

    Added login_required and error page

--- v2sh/contents/urls.py
+++ v2sh/contents/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url,include
 from contents import views
 
+
 urlpatterns = [
     url(r'^aboutus/$', views.aboutus , name= 'aboutus'),
     url(r'^reportissue/$', views.reportissue , name= 'reportissue'),
     url(r'^feedbackform/$',views.feedbackform, name= 'feedbackform'),
     url(r'^ourmission/$',views.ourmission, name= 'ourmission'),
     url(r'^bycompany/(?P<alpha>\w{0,50})$',views.bycompany,name = "bycompany")
+
 ]
--- v2sh/contents/views.py
+++ v2sh/contents/views.py
@@ -4,38 +4,46 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from superuser.models import SuperUser, Experience
+
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required()
 def home(request):
     return render(request, 'contents/home.html')
 
+@login_required()
 def aboutus(request):
-	return render(request, 'contents/aboutus.html')
+    return render(request, 'contents/aboutus.html')
 
+@login_required()
 def reportissue(request):
-	return render(request, 'contents/issue_report.html')
+    return render(request, 'contents/issue_report.html')
 
+@login_required()
 def feedbackform(request):
-	return render(request, 'contents/feedbackform.html')
+    return render(request, 'contents/feedbackform.html')
 
+@login_required()
 def ourmission(request):
-	return render(request, 'contents/ourmission.html')
+    return render(request, 'contents/ourmission.html')
 
+@login_required()
 def bycompany(request,alpha):
 
-	names=[]
-	number_of_people=[]
-	job_count_pairs = []
-	for i in range(Experience.objects.count()):
-		if(Experience.objects.all()[i].company_name[0]==alpha):
-			names.append(Experience.objects.all()[i].company_name)
-	for i in names:
-		cnt=0
-		for j in range(Experience.objects.count()):
-			if(str(i)== str(Experience.objects.all()[j].company_name)):
-				cnt+=1
-		number_of_people.append(cnt)
-	for i in range(len(names)):
-		job_count_pairs.append((names[i], number_of_people[i]))
+    names=[]
+    number_of_people=[]
+    job_count_pairs = []
+    for i in range(Experience.objects.count()):
+        if(Experience.objects.all()[i].company_name[0]==alpha):
+            names.append(Experience.objects.all()[i].company_name)
+    for i in names:
+        cnt=0
+        for j in range(Experience.objects.count()):
+            if(str(i)== str(Experience.objects.all()[j].company_name)):
+                cnt+=1
+        number_of_people.append(cnt)
+    for i in range(len(names)):
+        job_count_pairs.append((names[i], number_of_people[i]))
 
-	return render(request,'contents/bycompany.html',{'names':job_count_pairs,'alpha':alpha})
+    return render(request,'contents/bycompany.html',{'names':job_count_pairs,'alpha':alpha})
