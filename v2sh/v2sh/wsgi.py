cf1025f816f487ccdc31812bc507d3d74f23aeea
vishvanath45 <visitdutt@gmail.com>
2018-04-07 06:37:37 +0000

    unable to load static files: Fixed

--- v2sh/v2sh/wsgi.py
+++ v2sh/v2sh/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+#new added 1 line: problem unable to load static files
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "v2sh.settings")
 
 application = get_wsgi_application()
+#new added 1 line: problem unable to load static files
+application = DjangoWhiteNoise(application)
