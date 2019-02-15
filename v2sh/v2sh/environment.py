7be4b3ac23709cf46d9efcc60f931419b5421386
vishvanath45 <visitdutt@gmail.com>
2019-02-15 14:26:31 +0530

    Add .gitignore file

--- v2sh/v2sh/environment.py
+++ v2sh/v2sh/environment.py
@@ -1,20 +1,6 @@
 from pymongo import MongoClient
 client = MongoClient(IP)
 
-#Uncomment the environement you are working on !!!
-
-# Development Settings
-# DO NOT CHANGE !!
-# client.db_dev.authenticate(USERNAME,PASSWORD,mechanism='SCRAM-SHA-1')
-# db = client['db_dev']
-# experience = db['Experience']
-# superuser = db['SuperUser']
-# user = db['User']
-# credentials = db['credentials']
-# session = db['Session_table']
-
-# Production Settings
-# DO NOT CHANGE !!
 client.db_prod.authenticate(USERNAME,PASSWORD,mechanism='SCRAM-SHA-1')
 db = client['db_prod']
 experience = db['Experience']
@@ -22,12 +8,3 @@ superuser = db['SuperUser']
 user = db['User']
 credentials = db['credentials']
 session = db['Session_table']
-
-# Backup Settings
-# DO NOT CHANGE !!
-# client.db_backup.authenticate(USERNAME,PASSWORD,mechanism='SCRAM-SHA-1')
-# db = client['db_prod']
-# experience = db['Experience']
-# superuser = db['SuperUser']
-# user = db['User']'
-#credentials = db['credentials']
